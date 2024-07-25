04215c382006910fdf8c8c3882b397c97d4f90b5
Rohit Funde <rohit@ventii.jp>
2024-07-25 18:07:50 +0900

    init

---
+++ src/main/kotlin/jp/ventii/webfluxdemo/WebfluxDemoApplication.kt
@@ -0,0 +1,11 @@
+package jp.ventii.webfluxdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxDemoApplication>(*args)
+}
